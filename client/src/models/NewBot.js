cf3080ad4505bad53d98c1fe10b4dcebd1942c5f
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2016-11-28 14:49:00 +0000

    about ready to try to deploy

--- client/src/models/NewBot.js
+++ client/src/models/NewBot.js
@@ -9,6 +9,28 @@ var NewBot = function Constructor(settings) {
     this.settings.name = this.settings.name || 'Uninvited Robot';
     this.user = null;
     this.url = "https://wedding--photo-test.herokuapp.com/pictures";
+    this.jokes = [ "I bought some shoes off of a drug dealer. I don't know what he laced them with but I've been trippin' all day."
+
+        , "A mexican magician tells the audience he will disappear on the count of 3. He says, 'Uno, dos...' and then *poof* … he disappeared without a tres!"
+
+        , "What do you call the security guards outside of Samsung. The guardians of the galaxy!"
+
+        , "I think I want a job cleaning mirrors. It's something I could really see myself doing."
+
+        , "Why are there fences around a graveyard? Because people are dying to get in!"
+
+        , "Two satellites decided to get married. The wedding wasn't much, but the reception was incredible!"
+
+        , "What do you call a belt made out of watches? A waist of time!"
+
+        , "What kind of shoes does a pedophile wear? White vans."
+
+        , "What's the difference between a snow man and a snow woman? Snow balls!"
+
+        , "Did you hear about the guy who invented the knock knock joke?He won the 'no-bell' prize!"
+        , "Beep"
+
+        , "Bzzzzt" ]
 };
 
 util.inherits(NewBot, Bot);
@@ -34,45 +56,16 @@ NewBot.prototype._loadBotUser = function () {
 };
 
 NewBot.prototype._welcomeMessage = function () {
-    this.postMessageToChannel( this.channels[1].name, "Hello, I am a robot" );
+    this.postMessageToChannel( this.channels[0].name, "Hello, I am a robot" );
     this.haveSomeFun();
 };
 
-// NewBot.prototype._onMessage = function( message ) {
-//     if ( this._isChatMessage( message ) &&
-//      this._isChannelConversation( message ) &&
-//      !this._isFromNewBot( message )) {
-//         this._handleText( message );
-//     }
-// };
-
-// NewBot.prototype._handleText = function( message ) {
-//     if( message.text.charAt(0) !== '<' ) {
-//         this.postMessageToChannel( this.channels[1].name, "Hello, I am replying" );
-//     }
-// };
-
-// NewBot.prototype._handlePictures = function( message ) {
-//     if( message.subtype === 'file_share' ) {
-//         this._savePicture( message )
-//     }
-// };
-
-// NewBot.prototype._savePicture = function( message ) {
-//     this.postMessageToChannel( this.channels[1].name, "Hello, I am processing a picture" );
-//     var request = new XMLHttpRequest()
-//     request.open( 'POST', this.url )
-//     request.setRequestHeader("Content-Type", "application/json")
-//     request.onload = () => {
-//         console.log( this.url )
-//     }
-//     var data = {
-//         picture: {
-//           url: message.file.url_private
-//       }
-//     }
-//     request.send( JSON.stringify( data ));
-// };
+Newbot.prototype.haveSomeFun = function() {
+    setInterval( function() {
+        var i = Math.floor( Math.random() * ( max - min + 1 )) + min;
+        this.postMessageToChannel( this.channels[0].name, this.jokes[i] );
+    }.bind( this ), 1200000)
+}
 
 NewBot.prototype._isChatMessage = function ( message ) {
     return message.type === 'message' && Boolean( message.text );
